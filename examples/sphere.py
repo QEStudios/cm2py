59eaaa6fc775a41c126de000e28dd035993dc0a4
QEStudios <qestudios17@gmail.com>
2023-05-06 20:41:19 +1000

    Add sphere example code

---
+++ examples/sphere.py
@@ -0,0 +1,18 @@
+import cm2py as cm2
+import math
+
+radius = 5  # Radius of the sphere
+resolution = 10  # Number of divisions of the cube to generate
+block = cm2.OR  # Block ID to be used
+
+save = cm2.save()
+
+for r in range(resolution//2):
+    for i in range(resolution):
+        x = (math.sin(i/(resolution/(math.pi*2))))*radius * math.cos(r/(resolution/(math.pi*2)))
+        y = (math.cos(i/(resolution/(math.pi*2)))+1)*radius
+        z = (math.sin(r/(resolution/(math.pi*2))))*radius * math.sin(i/(resolution/(math.pi*2)))
+        save.addBlock(block, (x,y,z))
+
+saveString = save.export()
+print(saveString)
