abc74cd0ea33eb056bc575a8e2b00ae4a7968afb
QEStudios <qestudios17@gmail.com>
2023-05-08 21:45:19 +1000

    Change license to MIT

--- src/cm2py/cm2py.py
+++ src/cm2py/cm2py.py
@@ -3,18 +3,6 @@
 
 This module contains utilities to generate and manipulate save strings
 for the Roblox game Circuit Maker 2 by ismellbeef1.
-
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with
-this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 __author__ = "SKM GEEK"
@@ -23,7 +11,7 @@ __copyright__ = "Copyright 2023, SKM GEEK"
 __date__ = "2023/04/29"
 __deprecated__ = False
 __email__ =  "qestudios17@example.com"
-__license__ = "GPLv3"
+__license__ = "MIT"
 __maintainer__ = "SKM GEEK"
 __status__ = "Production"
 __version__ = "0.0.3"
