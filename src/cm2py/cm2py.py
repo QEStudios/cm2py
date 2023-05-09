0cd517c718a3b75ef0c72b3cf6a18beb0748ad44
QEStudios <qestudios17@gmail.com>
2023-05-09 22:26:24 +1000

    Formatting updates

--- src/cm2py/cm2py.py
+++ src/cm2py/cm2py.py
@@ -10,7 +10,7 @@ __contact__ = "qestudios17@gmail.com"
 __copyright__ = "Copyright 2023, SKM GEEK"
 __date__ = "2023/04/29"
 __deprecated__ = False
-__email__ =  "qestudios17@example.com"
+__email__ = "qestudios17@example.com"
 __license__ = "MIT"
 __maintainer__ = "SKM GEEK"
 __status__ = "Production"
@@ -19,8 +19,8 @@ __version__ = "0.0.3"
 import re
 from uuid import UUID, uuid4
 
+
 class save:
-    
     def __init__(self):
         self.blocks = []
         self.connections = {}
@@ -52,41 +52,52 @@ class save:
         connectionStrings = []
         for c in self.connections.values():
             for n in c:
-                connectionStrings.append(f"{[str(b.uuid) for b in self.blocks].index(str(n.source.uuid))+1},{[str(b.uuid) for b in self.blocks].index(str(n.target.uuid))+1}")
+                connectionStrings.append(
+                    (
+                        f"{[str(b.uuid) for b in self.blocks].index(str(n.source.uuid))+1},"
+                        f"{[str(b.uuid) for b in self.blocks].index(str(n.target.uuid))+1}"
+                    )
+                )
         saveString += ";".join(connectionStrings)
         return saveString
 
-class block:
 
+class block:
     def __init__(self, blockId, pos, state=False):
         assert isinstance(blockId, int) and 0 <= blockId <= 11, "blockId must be an integer between 0 and 11"
-        assert isinstance(pos, tuple) and len(pos) == 3 and (isinstance(pos[0], float) or isinstance(pos[0], int)) and (isinstance(pos[1], float) or isinstance(pos[1], int)) and (isinstance(pos[2], float) or isinstance(pos[2], int)), "pos must be a 3d tuple of integers"
+        assert (
+            isinstance(pos, tuple)
+            and len(pos) == 3
+            and (isinstance(pos[0], float) or isinstance(pos[0], int))
+            and (isinstance(pos[1], float) or isinstance(pos[1], int))
+            and (isinstance(pos[2], float) or isinstance(pos[2], int))
+        ), "pos must be a 3d tuple of integers"
         assert isinstance(state, bool), "state must be a boolean"
         self.blockId = blockId
-        self.pos = tuple([round(pos[i],2) for i in range(3)])
-        self.x = round(pos[0],2)
-        self.y = round(pos[1],2)
-        self.z = round(pos[2],2)
+        self.pos = tuple([round(pos[i], 2) for i in range(3)])
+        self.x = round(pos[0], 2)
+        self.y = round(pos[1], 2)
+        self.z = round(pos[2], 2)
         self.state = state
         self.uuid = uuid4()
 
-class connection:
 
+class connection:
     def __init__(self, source, target):
         assert isinstance(source, block) or isinstance(source, UUID), "source must be a Block object, or a UUID"
         assert isinstance(target, block) or isinstance(source, UUID), "target must be a Block object, or a UUID"
         self.source = source
         self.target = target
 
+
 def importSave(string, snapToGrid=True):
     """Import a Circuit Maker 2 save string as a save."""
-    # Full combined regex: r'^((\d+,){2}(-?\d+,){3}(((\d+)|(\d+\+){2}(\d+)))?;)+((\d+,){2}(-?\d+,){3}(((\d+)|(\d+\+){2}(\d+))?)\?)((([1-9][0-9]*),([1-9][0-9]*)|((([1-9][0-9]*),([1-9][0-9]*);)+([1-9][0-9]*),([1-9][0-9]*)))?)$'
     regex = (
         # Match all blocks
-        r'^((\d+,){2}(-?\d+,){3}(((\d+)|(\d+\+){2}(\d+)))?;)+'
-        r'((\d+,){2}(-?\d+,){3}(((\d+)|(\d+\+){2}(\d+))?)\?)'
+        r"^((\d+,){2}(-?\d+,){3}(((\d+)|(\d+\+){2}(\d+)))?;)+"
+        r"((\d+,){2}(-?\d+,){3}(((\d+)|(\d+\+){2}(\d+))?)\?)"
         # Match all connections
-        r'((([1-9][0-9]*),([1-9][0-9]*)|((([1-9][0-9]*),([1-9][0-9]*);)+([1-9][0-9]*),([1-9][0-9]*)))?)$'
+        r"((([1-9][0-9]*),([1-9][0-9]*)|((([1-9][0-9]*),([1-9][0-9]*);)+([1-9][0-9]*),([1-9][0-9]*)))?)$"
     )
 
     assert re.match(regex, string), "Invalid save string"
@@ -94,13 +105,19 @@ def importSave(string, snapToGrid=True):
     newSave = save()
 
     blocks = [[int(v) if v else None for v in i.split(",")] for i in "".join(string.split("?")[0]).split(";")]
-    connections = [[int(v) for v in i.split(",")] for i in "".join(string.split("?")[1]).split(";") if len("".join(string.split("?")[1]).split(";")) > 1 and isinstance("".join(string.split("?")[1]).split(";")[0], int) and isinstance("".join(string.split("?")[1]).split(";")[0], int)]
+    connections = [
+        [int(v) for v in i.split(",")]
+        for i in "".join(string.split("?")[1]).split(";")
+        if len("".join(string.split("?")[1]).split(";")) > 1
+        and isinstance("".join(string.split("?")[1]).split(";")[0], int)
+        and isinstance("".join(string.split("?")[1]).split(";")[0], int)
+    ]
     # Need to refactor this line
 
     for block in blocks:
         newSave.addBlock(block[0], (block[2], block[3], block[4]), state=bool(block[1]), snapToGrid=snapToGrid)
-    
+
     for connection in connections:
-        newSave.addConnection(blocks[connection[0]-1], blocks[connection[1]-1])
+        newSave.addConnection(blocks[connection[0] - 1], blocks[connection[1] - 1])
 
     return newSave
