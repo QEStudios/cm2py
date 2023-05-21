c9617d56b44a4cb87da5157dcd5e5fdea506c88e
QEStudios <qestudios17@gmail.com>
2023-05-21 15:33:05 +1000

    Get deleteBlock working

--- src/cm2py/cm2py.py
+++ src/cm2py/cm2py.py
@@ -67,6 +67,13 @@ class Save:
     def deleteBlock(self, blockRef):
         """Delete a block from the save."""
         assert isinstance(blockRef, Block), "blockRef must be a block object"
+        assert blockRef in self.blocks, "block does not exist in save"
+        for c in self.connections.values():
+            for n in c:
+                if n.source.uuid == blockRef.uuid or n.target.uuid == blockRef.uuid:
+                    del self.connections[str(n.target.uuid)][self.connections[str(n.target.uuid)].index(n)]
+                    break
+        del self.blocks[self.blocks.index(blockRef)]
         return
 
 
