80f301d9d488e4bb2acceace7367d629c632558f
QEStudios <qestudios17@gmail.com>
2023-07-10 14:24:35 +1000

    guess im updating the version number again

--- src/cm2py/cm2py.py
+++ src/cm2py/cm2py.py
@@ -14,7 +14,7 @@ __email__ = "qestudios17@example.com"
 __license__ = "MIT"
 __maintainer__ = "SKM GEEK"
 __status__ = "Production"
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 
 import re
 from uuid import UUID, uuid4
