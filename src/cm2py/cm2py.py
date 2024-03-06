d60e888480068fd3a76e8a8a726d1525a27d0b17
SKM GEEK <38995820+QEStudios@users.noreply.github.com>
2024-03-06 02:31:26 +0000

    Update version numbers

--- src/cm2py/cm2py.py
+++ src/cm2py/cm2py.py
@@ -14,7 +14,7 @@ __email__ = "qestudios17@gmail.com"
 __license__ = "MIT"
 __maintainer__ = "SKM GEEK"
 __status__ = "Production"
-__version__ = "0.3.5"
+__version__ = "0.3.6"
 
 from uuid import uuid4
 import math
