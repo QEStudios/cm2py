5f9ead8361ed6d68617aaf68ece8e274e1fa391f
SKM GEEK <38995820+QEStudios@users.noreply.github.com>
2024-03-06 01:43:58 +0000

    Update tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -17,10 +17,13 @@ def test_addBlocks():
     save.addBlock(cm2.CONDUCTOR, (8, 0, 0))
     save.addBlock(cm2.CUSTOM, (9, 0, 0))
     save.addBlock(cm2.NAND, (10, 0, 0))
-    save.addBlock(cm2.XNOR, (10, 0, 0))
-    save.addBlock(cm2.RANDOM, (10, 0, 0))
-    save.addBlock(cm2.TEXT, (10, 0, 0))
-    save.addBlock(cm2.TILE, (10, 0, 0))
+    save.addBlock(cm2.XNOR, (11, 0, 0))
+    save.addBlock(cm2.RANDOM, (12, 0, 0))
+    save.addBlock(cm2.TEXT, (13, 0, 0))
+    save.addBlock(cm2.TILE, (14, 0, 0))
+    save.addBlock(cm2.NODE, (15, 0, 0))
+    save.addBlock(cm2.DELAY, (16, 0, 0))
+    save.addBlock(cm2.ANTENNA, (17, 0, 0))
 
     save.exportSave()
 
@@ -83,6 +86,22 @@ def test_properties():
 
     save.exportSave()
 
+def test_move():
+    save = cm2.Save()
+
+    b1 = save.addBlock(cm2.OR, (1,2,3))
+    assert b1.pos == (1,2,3)
+    assert b1.x == 1 and b1.y == 2 and b1.z == 3
+
+    b1.pos = (4,5,6)
+    assert b1.pos == (4,5,6)
+    assert b1.x == 4 and b1.y == 5 and b1.z == 6
+
+    b1.x = 7
+    b1.y = 8
+    b1.z = 9
+    assert b1.pos == (7,8,9)
+    assert b1.x == 7 and b1.y == 8 and b1.z == 9
 
 def test_importSave():
     string = (
